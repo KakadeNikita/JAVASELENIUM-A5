cadeb4ecf4d2af149e160f7a088d2fb8d2a03237
Nikita Kakade <nikitakakade2324@gmail.com>
2025-07-20 21:56:52 +0530

    2nd commit

--- src/org/basic/LocatingElementSearchHumanBrain.java
+++ src/org/basic/LocatingElementSearchHumanBrain.java
@@ -1,14 +1,25 @@
 package org.basic;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class LocatingElementSearchHumanBrain {
 	public static void main(String[] args) {
-		WebDriver driver=new EdgeDriver();
+		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://www.amazon.in/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.get("https://www.flipkart.com/");
+		driver.findElement(By.name("q")).sendKeys("Watches for women",Keys.ENTER);
+		//String price = driver.findElement(By.xpath("//a[text()='Analog Watch  - For Women Latest Designer Printed Dial ...']/..//div[contains(text(),'₹')][1]")).getText();
+		//System.out.println(price);
+		driver.findElement(By.xpath("//a[text()='Analog Watch  - For Women Latest Designer Printed Dial ...']")).click();
+		
+		
 	}
 
 }
